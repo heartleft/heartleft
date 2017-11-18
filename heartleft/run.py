21466d180fc971f960c86b7a22112c51f126db49
zpzhou <himoker@163.com>
2017-11-18 21:12:12 +0800

    prepare

--- heartleft/run.py
+++ heartleft/run.py
@@ -1,4 +1,11 @@
 from heartleft.server import app
+from flask_script import Manager, Server
+
+manager = Manager(app)
+manager.add_command("runserver", Server('0.0.0.0', port=80))
 
 
-app.run(host='0.0.0.0', port=80, debug='true')
+# app.run(host='0.0.0.0', port=80, debug='true')
+
+def main():
+    manager.run()

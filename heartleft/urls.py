f878022095557c373b4b7699cf119eecba1210b2
zpzhou <himoker@163.com>
2017-11-18 20:59:28 +0800

    prepare

--- heartleft/urls.py
+++ heartleft/urls.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
-import modules
+import views
 
 m_hello_world = Blueprint('HelloWorld', __name__)
-m_hello_world.add_url_rule('/', 'index', modules.index, methods=['GET', 'POST'])
-m_hello_world.add_url_rule('/login', 'login', modules.login, methods=['GET', 'POST'])
+m_hello_world.add_url_rule('/', 'index', views.index, methods=['GET', 'POST'])
+m_hello_world.add_url_rule('/login', 'login', views.login, methods=['GET', 'POST'])
